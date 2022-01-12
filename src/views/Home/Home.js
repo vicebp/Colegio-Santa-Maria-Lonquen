e7bc572187a56143bf373cbf96a8a4a24262cccc
Vicente Berroeta <vinny.oc96@gmail.com>
2022-01-11 22:44:07 -0300

    welcome modified

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,11 @@
 import Navbar from "../navbar/Navbar";
+import Welcome from "./sections/welcome/Welcome";
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <div className="home-container">
-            <Navbar/>
+            <Navbar />
+            <Welcome />
         </div>
     )
 }
---
+++ src/views/Home/sections/welcome/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+
+export default function Welcome() {
+    return (
+        <Box backgroundImage="url('./assets/background-monte.svg')"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat">
+
+
+        </Box>
+    )
+}
